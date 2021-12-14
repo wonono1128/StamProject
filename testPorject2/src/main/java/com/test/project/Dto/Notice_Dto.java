590ff632c36e1ffb9ad2fe9f670ba8abed67cc2b
wonono1128 <aassddff1128@naver.com>
2021-12-14 09:41:03 +0900

    서드로 옮깁니다.

---
+++ testPorject2/src/main/java/com/test/project/Dto/Notice_Dto.java
@@ -0,0 +1,40 @@
+package com.test.project.Dto;
+
+public class Notice_Dto {
+
+	int nid;
+	String ntitle,ncontent,nwriter,nregdate;
+	
+	public int getNid() {
+		return nid;
+	}
+	public void setNid(int nid) {
+		this.nid = nid;
+	}
+	public String getNtitle() {
+		return ntitle;
+	}
+	public void setNtitle(String ntitle) {
+		this.ntitle = ntitle;
+	}
+	public String getNcontent() {
+		return ncontent;
+	}
+	public void setNcontent(String ncontent) {
+		this.ncontent = ncontent;
+	}
+	public String getNwriter() {
+		return nwriter;
+	}
+	public void setNwriter(String nwriter) {
+		this.nwriter = nwriter;
+	}
+	public String getNregdate() {
+		return nregdate;
+	}
+	public void setNregdate(String nregdate) {
+		this.nregdate = nregdate;
+	}
+	
+	
+}
